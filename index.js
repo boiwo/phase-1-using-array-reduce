773db9dd6407596e2199a1bfcbb5a8a31fd0ac33
benard <benardboiwo703@gmail.com>
2024-07-08 02:15:29 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
 
 // Code your solution here
+// index.js
+
+// Array containing the number of batteries assembled per batch
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
+// Function to calculate total batteries using reduce method
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+// Calculate total batteries using reduce
+const totalBatteries = batteryBatches.reduce(reducer, 0);
+
+// Export totalBatteries so it can be tested
+module.exports = totalBatteries;
